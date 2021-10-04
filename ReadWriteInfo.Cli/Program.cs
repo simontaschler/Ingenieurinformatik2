0ac45438be2542e082a120fa056754b05cf834a2
simontaschler <simon.taschler@infominds.eu>
2021-10-04 20:15:16 +0200

    Aufgabe 3: Read and write strings

---
+++ ReadWriteInfo.Cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReadWriteInfo.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first name and press Return");
+            var firstName = Console.ReadLine();
+            Console.WriteLine("Enter last name and press Return");
+            var lastName = Console.ReadLine();
+            Console.WriteLine("Enter address and press Return");
+            var address = Console.ReadLine();
+            Console.WriteLine($"{firstName}_{lastName}, {address}");
+        }
+    }
+}
