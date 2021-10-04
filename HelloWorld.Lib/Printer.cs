3e571a57c18e77710326f3bbc53320c7a9c7aa9f
simontaschler <simon.taschler@infominds.eu>
2021-10-04 20:16:53 +0200

    Aufgabe 2: Added different output messages

--- HelloWorld.Lib/Printer.cs
+++ HelloWorld.Lib/Printer.cs
@@ -5,6 +5,6 @@ namespace HelloWorld.Lib
     public static class Printer
     {
         public static void PrintHelloWorld() => 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! (from class within other project and namespace)");
     }
 }
