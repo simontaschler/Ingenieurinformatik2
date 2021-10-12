1ec552e765fdfe06cc5e154723bb1273ef562b33
simontaschler <simon.taschler@infominds.eu>
2021-10-12 14:57:54 +0200

    U2 Aufgabe 1

---
+++ U2/U2/BonusPoints.Cli/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BonusPoints.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.WriteLine("Enter first player's points");
+                var player1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second player's points");
+                var player2 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter third player's points");
+                var player3 = int.Parse(Console.ReadLine());
+
+                var sum = player1 + player2 + player3;
+                Console.WriteLine($"Total of {sum} points");
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("Error: Input did not numeric format");
+            }
+            catch (OverflowException) 
+            {
+                Console.WriteLine("Error: Input out of bounds of Int32");
+            }
+        }
+    }
+}
