2a2154125cbdb442da7b5fc25fe7124894ac2ce5
simontaschler <simon.taschler@infominds.eu>
2021-10-13 18:41:41 +0200

    U2 Aufgabe 4: fixed assignment to a

--- U2/Flowchart.Cli/Program.cs
+++ U2/Flowchart.Cli/Program.cs
@@ -9,45 +9,49 @@ namespace Flowchart.Cli
             Console.WriteLine("Enter an integer between 1 and 4");
             var input = Console.ReadLine();
 
-            if (int.TryParse(input, out var a))
+            if (int.TryParse(input, out var _a))
             {
+                double a = _a;
                 switch (a) 
                 {
-                    case 1: Console.WriteLine(a + 1); break;
-                    case 2: Console.WriteLine(a % 2); break;
-                    case 3: Console.WriteLine(a / 2); break;
+                    case 1: a++; break;
+                    case 2: a %= 2; break;
+                    case 3: a = (int)(a / 2); break;
                         //Nachkommastelle(n) werden ignoriert und verworfen durch ganzzahligen Datentyp
-                    case 4: Console.WriteLine(Math.Pow(a, 32).ToString("N0")); break;
+                    case 4: a = Math.Pow(a, 32); break;
                         //4^32 = 2^64, was größer als MaxValue vom standardmäßigen Int32 ist
                         //auch höchste MaxValue eines ganzzahligen Datentyps UInt64 ist nur 2^64 - 1,
                         //deshalb muss double verwendet werden
                     default: Console.WriteLine("Error: Your input is out of bounds"); break;
                 }
+
+                if (_a >= 1 && _a <= 4)
+                    Console.WriteLine($"a = {a.ToString("N0")}");
+
+                Console.WriteLine("Enter an integer");
+                input = Console.ReadLine();
+
+                if (int.TryParse(input, out var b))
+                {
+                    if (a % 2 == 0)
+                    {
+                        Console.WriteLine("Even");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Odd");
+                        Console.WriteLine(a > b
+                            ? "a greater than b"
+                            : "a less than b");
+                    }
+                    Console.WriteLine($"b = {b}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: Your input did not match valid format.");
+                }
             }
             else 
-            {
-                Console.WriteLine("Error: Your input did not match valid format.");
-            }
-
-            Console.WriteLine("Enter an integer");
-            input = Console.ReadLine();
-
-            if (int.TryParse(input, out var b)) 
-            {
-                if (a % 2 == 0) 
-                {
-                    Console.WriteLine("Even");
-                }
-                else 
-                {
-                    Console.WriteLine("Odd");
-                    Console.WriteLine(a > b
-                        ? "a greater than b"
-                        : "a less than b");
-                }
-                Console.WriteLine(b);
-            }
-            else
             {
                 Console.WriteLine("Error: Your input did not match valid format.");
             }
