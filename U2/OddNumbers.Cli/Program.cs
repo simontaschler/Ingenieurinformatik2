7a055c646f23376ae4b2029e3cad2b786f5d9104
simontaschler <simon.taschler@infominds.eu>
2021-10-13 13:12:46 +0200

    U2 Aufgabe 3

---
+++ U2/OddNumbers.Cli/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddNumbers.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oddNumbers = new List<int>();
+            for (var i = 1; i <= 21; i += 2)
+                oddNumbers.Add(i);
+
+            var output = string.Join(' ', oddNumbers);
+            oddNumbers.Reverse();
+            output += $" {string.Join(' ', oddNumbers)}";
+            Console.WriteLine(output);
+        }
+    }
+}
