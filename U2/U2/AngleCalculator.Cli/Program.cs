3f95ee5758565e93495b5a5e85176f78b4d76c63
simontaschler <simon.taschler@infominds.eu>
2021-10-13 12:58:55 +0200

    U2 Aufgabe 2

---
+++ U2/U2/AngleCalculator.Cli/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace AngleCalculator.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region double
+            {
+                var hypotenuse = 10.0;
+                var ankathete = 5.0;
+                var cos = ankathete / hypotenuse;
+                Console.WriteLine($"Double calculation: Enclosed Angle of {Math.Acos(cos)} rad");
+            }
+            #endregion
+
+            #region integer
+            {
+                var hypotenuse = 10;
+                var ankathete = 5;
+                var cos = ankathete / hypotenuse;
+                Console.WriteLine($"Int calculation: Enclosed Angle of {Math.Acos(cos)} rad");
+            }
+            #endregion
+        }
+    }
+}
