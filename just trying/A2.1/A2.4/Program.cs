0ddf28659d787853d47b8c7056bcbd2bd540953a
Stefan Allmayer <stefan.allmayer@gmail.com>
2021-10-15 14:49:17 +0200

    Aufgabe 4 fertiggestellt

--- just trying/A2.1/A2.4/Program.cs
+++ just trying/A2.1/A2.4/Program.cs
@@ -13,7 +13,8 @@ namespace A2._4
             Console.WriteLine("Bitte geben Sie einen Parameter a als Integer ein um fortzufahren");
             var input = Console.ReadLine();
             int a = int.Parse(input);
-
+                   
+            
             if (a == 1)
             {
                 a = a + 1;
@@ -40,6 +41,7 @@ namespace A2._4
                             var converteda = (double)a;
                             converteda = Math.Pow(a, 32);
                             Console.WriteLine("a=" + converteda.ToString("N0"));
+                            
                         }
                         else
                         {
@@ -47,6 +49,31 @@ namespace A2._4
                         }
                     }
                 } 
+            }
+            
+            // converted a fehlt aber da 4^32 immer gerade ist (modulo = 0) stimmt das Ergebnis der Abfrage b trotzdem)
+
+            Console.WriteLine("Geben Sie einen Paramaeter b als Integer ein um fortzufahren");
+
+            var binput = Console.ReadLine();
+            var b = int.Parse(binput);
+
+            if (a%2 == 0)
+            {
+                Console.WriteLine("Gerade");
+            }
+            else
+            {
+                Console.WriteLine("Ungerade");
+                if (a > b)
+                {
+                    Console.WriteLine("a ist größer als b");
+                }
+                else
+                {
+                    Console.WriteLine("logisch richtige Aussage");
+                }
+                   
             }
 
             Console.ReadLine();
