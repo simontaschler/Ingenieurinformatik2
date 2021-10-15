b6588d43510aa4a8f9e727e04368df30c02da39c
stefan.allmayer <mist.tv@gmail.com>
2021-10-15 09:50:27 +0200

    Stefans Versuche des zweiten Arbeitsblattes

---
+++ just trying/A2.1/A2.3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Erstellen einer Liste in der die ungeraden Zahlen aufsteigend geordnet sind, und erstellen einer Laufvariablen für die ungeraden Zahlen
+            var Liste = new List<int>();
+            var ungeradeZahlen = 1;
+
+        //Die for Schleife läuft in Ihrer Laufvariablen i von 0-10. In jedem Schritt der Schleife wird die variable der ungeraden Zahlen um 2 erhöht und der Liste hinzugefügt.
+            for (int i = 0; i <= 10; ungeradeZahlen = ungeradeZahlen + 2, i = i + 1)
+                Liste.Add(ungeradeZahlen);
+
+        //Um die Liste in der Konsole ausgeben zu können, wird sie in einen String konvertiert. Hierbei wird der String " " als Seperator verwendet (Leerzeichen zwischen den Elementen)
+            string output1 = string.Join(" " , Liste);
+         
+        //Umkehren der Liste in abfallende Reihenfolge
+            Liste.Reverse();
+
+        //Konvertieren der abfallenden Liste in einen String
+            string output2 = string.Join(" " , Liste);
+
+        //Ausgabe über die Konsole
+            Console.WriteLine(output1 + " " + output2);
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ just trying/A2.1/A2.4/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte geben Sie einen Parameter a als Integer ein um fortzufahren");
+            var input = Console.ReadLine();
+            int a = int.Parse(input);
+
+            if (a == 1)
+            {
+                a = a + 1;
+                Console.WriteLine("a=" + a);
+            }
+            else
+            {
+                if (a == 2)
+                {
+                    a = a % 2;
+                    Console.WriteLine("a=" + a);
+                }
+                else
+                {
+                    if (a == 3)
+                    {
+                        a = a / 2;
+                        Console.WriteLine("a=" + a);
+                    }
+                    else
+                    {
+                        if (a == 4)
+                        {
+                            var converteda = (double)a;
+                            converteda = Math.Pow(a, 32);
+                            Console.WriteLine("a=" + converteda.ToString("N0"));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nicht definiert");
+                        }
+                    }
+                } 
+            }
+
+            Console.ReadLine();
+                
+           
+        }
+    }
+}
