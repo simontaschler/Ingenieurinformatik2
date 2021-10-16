2a2430e91fcb93ad020ea1c2d64fb66239b762cf
simontaschler <simon.taschler@infominds.eu>
2021-10-16 16:44:48 +0200

    U3 Aufgabe 4

---
+++ U3/U3/NumberTriangle.Cli/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberTriangle.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentNumber = 0;
+            var numCollection = new List<int>();
+
+            for (var i = 1; i <= 25; i++)
+            {
+                currentNumber += i;
+                numCollection.Add(currentNumber);
+            }
+
+            Console.WriteLine(string.Join(", ", numCollection.Where(q => q % 5 == 0)));
+            Console.ReadLine();
+        }
+    }
+}
