dad19e90384b9a6cde965d032231a951dd6590a7
simontaschler <simon.taschler@infominds.eu>
2021-10-24 15:49:08 +0200

    U4 Aufgabe 6

---
+++ U4/SongManager.Cli/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SongManager.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var songs = new Song[]
+            {
+                new Song("White Ferrari", "Frank Ocean", new TimeSpan(0, 4, 9)),
+                new Song("Detach", "Hans Zimmer", new TimeSpan(0, 6, 42)),
+                new Song("Troublemaker", "Beach House", new TimeSpan(0, 4, 9))
+            };
+
+            foreach (var song in songs)
+                Console.WriteLine(song);
+
+            //Alternativen:
+            //for (var i = 0; i < 3; i++)
+            //    Console.WriteLine(songs[i]);
+
+            //Console.WriteLine(string.Join(Environment.NewLine, songs as object[]));
+
+            Console.ReadLine();
+        }
+    }
+}
