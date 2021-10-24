d7f15747d83ee5ef54c00cdfd69ecee8b8ed7d4f
simontaschler <simon.taschler@infominds.eu>
2021-10-24 22:01:12 +0200

    U4 Aufgabe 7

---
+++ U4/Toolbox.Cli/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolbox.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var toolbox = new Toolbox
+            {
+                Name = "Werkzeugkasten",
+                Count = 4
+            };
+
+            toolbox.Add(new Tool
+            {
+                Name = "Hammer",
+                Price = 16.90F
+            });
+            toolbox.Add(new Tool
+            {
+                Name = "Schraubenzieher",
+                Price = 14.90F
+            });
+            toolbox.Add(new Tool
+            {
+                Name = "Zange",
+                Price = 19.90F
+            });
+            toolbox.Add(new Tool
+            {
+                Name = "Akkuschrauber",
+                Price = 49.90F
+            });
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine($"Gesamtpreis: {toolbox.GetTotalPrice():C2}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ U4/Toolbox.Cli/Tool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Toolbox.Cli
+{
+    internal class Tool
+    {
+        //MSBuild erstellt bei Properties automatisch im Hintergrund Getter- und Setter-Methoden
+        //z.B. internal string get_Name() bzw. internal void set_Name(string value)
+        //wenn diese Methoden nicht überschrieben werden, wird auch privates Feld angelegt, das den Wert speichert
+        internal string Name { get; set; }
+        internal float Price { get; set; }
+    }
+}
