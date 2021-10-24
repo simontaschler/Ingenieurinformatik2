b665bac43df07afba17da4761a4ae8a7acd04729
simontaschler <simon.taschler@infominds.eu>
2021-10-24 15:28:18 +0200

    U4 Aufgabe 5

---
+++ U4/Drinks.Cli/Drink.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drinks.Cli
+{
+    internal class Drink
+    {
+        private static int nextID = 1;
+
+        internal int ID { get; }
+        internal string Name { get; set; }
+        internal float Alcohol { get; set; }
+        internal bool Mixable { get; set; }
+
+        public Drink()
+        {
+            ID = nextID;
+            nextID++;
+        }
+
+        public override string ToString() =>
+            new StringBuilder($"  {ID}".PadRight(12))
+            .Append(Name.PadRight(12))
+            .Append($"{Alcohol}".PadRight(12))
+            .Append($"{Mixable}".PadRight(12))
+            .ToString();
+    }
+}
---
+++ U4/Drinks.Cli/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drinks.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drinks = new List<Drink>
+            {
+                new Drink
+                {
+                    Name = "Kaffee",
+                    Alcohol = 0,
+                    Mixable = false
+                },
+                new Drink
+                {
+                    Name = "Eistee",
+                    Alcohol = 0,
+                    Mixable = false
+                },
+                new Drink
+                {
+                    Name = "Bier",
+                    Alcohol = 5.2F,
+                    Mixable = true
+                },
+                new Drink
+                {
+                    Name = "Weißwein",
+                    Alcohol = 12.5F,
+                    Mixable = true
+                },
+                new Drink
+                {
+                    Name = "Milch",
+                    Alcohol = 0,
+                    Mixable = true
+                }
+            };
+
+            Console.WriteLine("Lagernr.   Name     Alkoholgehalt  Mit Cola mischbar?");
+            Console.WriteLine(string.Join(Environment.NewLine, drinks));
+            Console.ReadLine();
+        }
+    }
+}
