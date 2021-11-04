c692d14f30b7a7746a15e2efa16a2625ddac8c98
stefan.allmayer <mist.tv@gmail.com>
2021-11-04 22:28:51 +0100

    4.2 funktioniert aber nur mit if break ... while argument stoppt nicht???

--- just trying/Arbeitsblatt4Stevewonder/4.2/Program.cs
+++ just trying/Arbeitsblatt4Stevewonder/4.2/Program.cs
@@ -14,10 +14,115 @@ namespace _4._2
             int[,] loesung = EinlesenDerLoesung();
             int[,] spielvorgabe = EinlesnDerSpielvorlage();
 
+            
+            //Ausgabe des aktuellen Spielstandes über die Konsole
+            int i = 0;
+            int[] Zeile1 = new int[6];
+            int[] Zeile2 = new int[6];
+            int[] Zeile3 = new int[6];
+            int[] Zeile4 = new int[6];
+            int[] Zeile5 = new int[6];
+            int[] Zeile6 = new int[6];
+            int[] inputLoesung = { 1, 6, 4, 5, 3, 2, 5, 2, 3, 6, 1, 4, 4, 1, 2, 3, 6, 5, 3, 5, 6, 2, 4, 1, 6, 4, 5, 1, 2, 3, 2, 3, 1, 4, 5, 6 };
+            int[] spielstandvergleich = new int[35];
+            spielstandvergleich = (Zeile1.Concat(Zeile2).Concat(Zeile3).Concat(Zeile4).Concat(Zeile5).Concat(Zeile6)).ToArray();
+            
+            while (!inputLoesung.SequenceEqual(spielstandvergleich)) //Vergleich funktioniert nicht?? deshalb if break....
+            {
+                do
+                {
+                    Zeile1[i] = spielvorgabe[0, i];
+                    i++;
+                }
+                while (i <= 5); //output Zeile 1
+                string outputZeile1 = string.Join(" ", Zeile1);
+                Console.WriteLine(outputZeile1);
+                i = 0;
 
-            string Spielstand= string.Join("|", spielvorgabe);
-            Console.Write(Spielstand);
-            Console.ReadLine();
+                do
+                {
+                    Zeile2[i] = spielvorgabe[1, i];
+                    i++;
+                }
+                while (i <= 5); //output Zeile 2
+                string outputZeile2 = string.Join(" ", Zeile2);
+                Console.WriteLine(outputZeile2);
+                i = 0;
+
+                do
+                {
+                    Zeile3[i] = spielvorgabe[2, i];
+                    i++;
+                }
+                while (i <= 5); //output Zeile 3
+                string outputZeile3 = string.Join(" ", Zeile3);
+                Console.WriteLine(outputZeile3);
+                i = 0;
+
+                do
+                {
+                    Zeile4[i] = spielvorgabe[3, i];
+                    i++;
+                }
+                while (i <= 5); //output Zeile 4
+                string outputZeile4 = string.Join(" ", Zeile4);
+                Console.WriteLine(outputZeile4);
+                i = 0;
+
+                do
+                {
+                    Zeile5[i] = spielvorgabe[4, i];
+                    i++;
+                }
+                while (i <= 5); //output Zeile 5
+                string outputZeile5 = string.Join(" ", Zeile5);
+                Console.WriteLine(outputZeile5);
+                i = 0;
+
+                do
+                {
+                    Zeile6[i] = spielvorgabe[5, i];
+                    i++;
+                }
+                while (i <= 5); //output Zeile 6
+                string outputZeile6 = string.Join(" ", Zeile6);
+                Console.WriteLine(outputZeile6);
+                i = 0;
+                
+                spielstandvergleich = (Zeile1.Concat(Zeile2).Concat(Zeile3).Concat(Zeile4).Concat(Zeile5).Concat(Zeile6)).ToArray();
+                if (inputLoesung.SequenceEqual(spielstandvergleich))
+                    break;
+
+
+                string inRow;
+                string inCollumn;
+                string inNumber;
+                Console.WriteLine("Geben Sie Ihren nächsten Spielzug ein. Zeile:");
+                inRow = Console.ReadLine();
+                Console.WriteLine("Reihe");
+                inCollumn = Console.ReadLine();
+                Console.WriteLine("Loesungszahl");
+                inNumber=Console.ReadLine();
+
+                int loesungscheck = loesung[(int.Parse(inRow))-1, (int.Parse(inCollumn))-1];
+                if (int.Parse(inNumber) == loesungscheck)
+                {
+                    spielvorgabe[int.Parse(inRow)-1, int.Parse(inCollumn)-1] = int.Parse(inNumber);
+                    Console.Clear();
+                    Console.WriteLine("TipTop Spielzug!");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Leider falsch...");
+                }
+                
+                
+            }
+
+            Console.Clear();
+            Console.WriteLine("Gratuliere Ihnen zum Sieg!");
+            Console.ReadLine();    
 
         }
         static int[,] EinlesenDerLoesung()
