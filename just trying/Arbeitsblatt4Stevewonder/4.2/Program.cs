9bb835149d62a658c171ec2684448beb8fc83a28
stefan.allmayer <mist.tv@gmail.com>
2021-11-04 18:01:09 +0100

    Einlesen funktioniert soweit

--- just trying/Arbeitsblatt4Stevewonder/4.2/Program.cs
+++ just trying/Arbeitsblatt4Stevewonder/4.2/Program.cs
@@ -10,9 +10,13 @@ namespace _4._2
     {
         static void Main(string[] args)
         {
-            
+
             int[,] loesung = EinlesenDerLoesung();
-            Console.WriteLine(loesung[1, 1]);
+            int[,] spielvorgabe = EinlesnDerSpielvorlage();
+
+
+            string Spielstand= string.Join("|", spielvorgabe);
+            Console.Write(Spielstand);
             Console.ReadLine();
 
         }
@@ -20,12 +24,11 @@ namespace _4._2
         {
             int[,] loesungSudoku = new int[6, 6];
             int[] inputLoesung = { 1, 6, 4, 5, 3, 2, 5, 2, 3, 6, 1, 4, 4, 1, 2, 3, 6, 5, 3, 5, 6, 2, 4, 1, 6, 4, 5, 1, 2, 3, 2, 3, 1, 4, 5, 6 };
-            string[,] arbeitsSudoku = new string[6, 6];
 
             int i = 1;
 
-            int row = 1;
-            int collumn = 1;
+            int rowcount = 0;
+            int collumncount = 0;
 
 
             //Schleife zum Einlesen des Loesungssudoku
@@ -34,15 +37,15 @@ namespace _4._2
 
                 if (i % 6 == 0)
                 {
-                    loesungSudoku[row - 1, collumn - 1] = inputLoesung[i - 1];
-                    row++;
-                    collumn = 1;
+                    loesungSudoku[rowcount, collumncount] = inputLoesung[i - 1];
+                    rowcount++;
+                    collumncount = 0;
                     i++;
                 }
                 else
                 {
-                    loesungSudoku[row - 1, collumn - 1] = inputLoesung[i - 1];
-                    collumn++;
+                    loesungSudoku[rowcount, collumncount] = inputLoesung[i - 1];
+                    collumncount++;
                     i++;
                 }
 
@@ -50,7 +53,39 @@ namespace _4._2
             return loesungSudoku;
         }
 
-            
+        static int[,] EinlesnDerSpielvorlage()
+        {
+            int[,] spielvorgabeSudoku = new int[6, 6];
+            int[] inputSpielvorgabe = {1, 6, 0, 5, 0, 2, 5, 0, 3, 0, 1, 4, 0, 1, 2, 0, 6, 0, 3, 0, 6, 2, 0, 1, 6, 0, 0, 0, 2, 0, 2, 3, 1, 4, 0, 6};
+           
+            int i = 1;
+
+            int row = 0;
+            int collumn = 0;
+
+
+            //Schleife zum Einlesen der Spielvorgabe
+            while (i <= 36)
+            {
+
+                if (i % 6 == 0)
+                {
+                    spielvorgabeSudoku[row, collumn] = inputSpielvorgabe[i - 1];
+                    row++;
+                    collumn = 0;
+                    i++;
+                }
+                else
+                {
+                    spielvorgabeSudoku[row, collumn] = inputSpielvorgabe[i - 1];
+                    collumn++;
+                    i++;
+                }
+
+            }
+            return spielvorgabeSudoku;
+
+        }
     }
 
 }
