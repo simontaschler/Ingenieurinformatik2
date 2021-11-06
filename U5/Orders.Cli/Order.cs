b81b6bd0a8b8a74243d67758c4776e539b1d1341
simontaschler <simon.taschler@infominds.eu>
2021-11-06 21:56:20 +0100

    U5 Aufgabe 4

---
+++ U5/Orders.Cli/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Cli
+{
+    internal class Order
+    {
+        private static int nextID = 1;
+
+        private readonly string Name;
+        private readonly float Price;
+        private readonly int ID;
+    
+        internal Order(string name, float price = 0F /*optionaler Parameter, wird wenn nicht übergeben mit gegebenem Wert initialisiert*/) 
+        {
+            Name = name;
+            Price = price;
+            ID = nextID;
+            nextID++;
+        }
+
+        internal Order(float price = 0F) 
+        {
+            Name = "???";
+            Price = price;
+            ID = nextID;
+            nextID++;
+        }
+
+        public override string ToString() =>
+            $"{ID}: {Name}, {Price:C2}";
+    }
+}
---
+++ U5/Orders.Cli/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Orders.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine(new Order("Asia Wok", 6.9F));
+            Console.WriteLine(new Order("Hamburger", 10F));
+            Console.WriteLine(new Order(5F));
+            Console.WriteLine(new Order("Kuchen"));
+            Console.WriteLine(new Order("Lasagna", 4.2F));
+        }
+    }
+}
