22d58b648f7e2a07876d067ff64a72a9ea436a7a
simontaschler <simon.taschler@infominds.eu>
2021-11-06 21:34:09 +0100

    U5 Aufgabe 2

---
+++ U5/GreatestCommonFactor.Cli/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GreatestCommonFactor.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 98;
+            var b = 56;
+
+            Console.WriteLine($"Recursive Calculation: Greatest common factor between {a} and {b} is {GCF_recursive(a,b)}");
+            Console.WriteLine($"Iterative Calculation: Greatest common factor between {a} and {b} is {GCF_iterative(a,b)}");
+        }
+
+        static int GCF_recursive(int a, int b) 
+        {
+            if (b == 0)
+                return a;
+            return GCF_recursive(b, a % b);
+        }
+
+        static int GCF_iterative(int a, int b)
+        {
+            if (a == b)
+            {
+                return a;
+            }
+            else if (b > a) 
+            {
+                var cache = a;
+                a = b;
+                b = cache;
+            }
+
+            int i;
+            for (i = b; a % i != 0 || b % i != 0; i--);
+            return i;
+        }
+
+    }
+}
