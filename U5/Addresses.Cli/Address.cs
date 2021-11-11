6d6bce793227be1319dd134345bc25c0c5ed74ab
simontaschler <simon.taschler@infominds.eu>
2021-11-11 17:55:04 +0100

    U5 Aufgabe 3

---
+++ U5/Addresses.Cli/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addresses.Cli
+{
+    internal class Address
+    {
+        internal string FirstName { get; set; }
+        internal string LastName { get; set; }
+        internal string Road { get; set; }
+        internal int ZipCode { get; set; }
+        internal string Town { get; set; }
+
+        internal string FullName => 
+            $"{FirstName} {LastName}";
+
+        public override string ToString() =>
+            $@"{FirstName} {LastName}:
+    {Road}, {ZipCode} {Town}";
+    }
+}
