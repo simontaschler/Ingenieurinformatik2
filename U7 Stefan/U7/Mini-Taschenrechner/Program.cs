a16f198a84930be4031bacae725f7a894dfeb585
stefan.allmayer <mist.tv@gmail.com>
2021-11-24 09:49:36 +0100

    7.1 und 7.2 Stefan fertig und unkommentiert

---
+++ U7 Stefan/U7/Mini-Taschenrechner/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Taschenrechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double input1;
+            double input2;
+            string inputOperator;
+            double ergebnis;
+            
+            
+            Console.WriteLine("**********Taschenrechner**********");
+        Eingabe1:
+            try 
+            {                
+                Console.WriteLine("Geben Sie die erste Zahl ein");
+                input1 = double.Parse(Console.ReadLine());
+            }
+            catch(SystemException)
+            {
+                Console.WriteLine("Ungültige Eingabe! Nur Zahlen erlaubt");
+                goto Eingabe1;
+            }
+            Console.Clear();
+        Eingabe2:
+            try
+            {
+                Console.WriteLine("Geben Sie die zweit Zahl ein");
+                input2 = double.Parse(Console.ReadLine());
+            }
+            catch(SystemException)
+            {
+                Console.WriteLine("Ungültige Eingabe! Nur Zahlen erlaubt");
+                goto Eingabe2;
+            }
+            Console.Clear();
+        EingabeOperator:
+            Console.WriteLine("Geben Sie einen Rechenoperator ein!");
+            inputOperator = Console.ReadLine();
+            switch(inputOperator)
+            {
+                case "*":
+                    ergebnis = input1 * input2;
+                    break;
+                case "/":
+                    if (input2 == 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Division durch 0!");
+                        goto Eingabe1;
+                    }  
+                    else
+                    {
+                        ergebnis = input1 / input2;
+                    }
+                    break;
+                case "+":
+                    ergebnis = input1 + input2;
+                    break;
+                case "-":
+                    ergebnis = input1 - input2;
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Ungültige Eingabe! Zulässig sind * / + und -");
+                    goto EingabeOperator;                   
+            }
+            Console.WriteLine($"{input1} {inputOperator} {input2} = {ergebnis}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ U7 Stefan/U7/U7/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+        Start:
+            Console.Clear();
+            int i = 0;
+            Console.WriteLine(@"Wählen Sie ein Program:
+1x1 lernen              [1]
+Zufallszahl ausgeben    [2]
+Program beenden         [3]");
+            try
+            {
+                input = int.Parse(Console.ReadLine());
+            }
+            catch (SystemException)
+            {
+                Console.Clear();
+                Console.WriteLine("Ungültige Eingabe");
+                goto Start;
+            }
+            switch(input)
+            {
+                case 1:
+                    while (i <= 10)
+                    {
+                       
+                    FalscheAntwort:
+                        int antwort;
+                        try
+                        {
+                            Console.WriteLine($"{i} * 7 = ?");
+                            antwort = int.Parse(Console.ReadLine());                           
+                        }
+                        catch(SystemException)
+                        {
+                            Console.WriteLine("Ungültige Eingabe");
+                            goto FalscheAntwort;
+                        }
+                        if (antwort == i * 7)
+                        {
+                            Console.WriteLine("Bravo!");
+                            i++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Falsche Antwort... Versuch's nochmal");
+                            goto FalscheAntwort;
+                        }                    
+                    }
+                    Console.Clear();
+                    Console.WriteLine("Geschafft! Du Mathekönig!!\nPress any key to continue");
+                    Console.ReadLine();
+                    goto Start;
+                    
+                case 2:
+                    Random rnd = new Random();
+                    int randomNumber = rnd.Next(1, 10);
+                    Console.WriteLine($@"Zufallszahl: {randomNumber}
+Press any key to continue!");
+                    Console.ReadLine();
+                    goto Start;
+                case 3:
+                    return;
+                default:
+                    Console.Clear();
+                    Console.WriteLine($"Ungültige Eingabe: {input}! nur Zahlen zwischen 1 und 2 zulässig!");
+                    goto Start;                  
+            }      
+        }
+    }
+}
