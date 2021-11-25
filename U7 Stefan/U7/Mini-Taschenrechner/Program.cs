44a4dcaaa0fca86aac4134a691d5f3782db788b5
Stefan Allmayer <stefan.allmayer@gmail.com>
2021-11-25 16:20:32 +0100

    Kommentare zu 7.1 und 7.2

--- U7 Stefan/U7/Mini-Taschenrechner/Program.cs
+++ U7 Stefan/U7/Mini-Taschenrechner/Program.cs
@@ -10,27 +10,27 @@ namespace Mini_Taschenrechner
     {
         static void Main(string[] args)
         {
-            decimal input1;
+            decimal input1;     //Festlegen der input Werte als decimal da dividiedbyZero Exception auf decimal ausgelegt sind
             decimal input2;
             string inputOperator;
             decimal ergebnis;
             
         Start:  
             Console.WriteLine("**********Taschenrechner**********");
-        Eingabe1:
-            try 
+        Eingabe1:   //Sprungmarke Eingabe 1
+            try         //Try catch zum Abfangen möglicher falscher inputs (non numeric)
             {                
                 Console.WriteLine("Geben Sie die erste Zahl ein");
-                input1 = decimal.Parse(Console.ReadLine());
+                input1 = decimal.Parse(Console.ReadLine());     //Parsen des input strings als decimal
             }
-            catch(FormatException)
+            catch(FormatException)  //Format Exception falls non nummeric input
             {
                 Console.WriteLine("Ungültige Eingabe! Nur Zahlen erlaubt");
-                goto Eingabe1;
+                goto Eingabe1;      //gehe zur Sprungmarke Eingabe1 falls die Eingabe ungültig war
             }
             Console.Clear();
-        Eingabe2:
-            try
+        Eingabe2:   //Sprungmarke Eingabe 2
+            try         //Äquivalent zur Eingabe des 1. Parameters
             {
                 Console.WriteLine("Geben Sie die zweit Zahl ein");
                 input2 = decimal.Parse(Console.ReadLine());
@@ -41,16 +41,16 @@ namespace Mini_Taschenrechner
                 goto Eingabe2;
             }
             Console.Clear();
-        EingabeOperator:
+        EingabeOperator:        //Sprungmarke EingabeOperator
             Console.WriteLine("Geben Sie einen Rechenoperator ein!");
             inputOperator = Console.ReadLine();
-            switch(inputOperator)
+            switch(inputOperator)       //Switch um die jeweilige Rechenoperation auszuführen
             {
                 case "*":
                     ergebnis = input1 * input2;
                     break;
                 case "/":                   
-                     try
+                     try        //Bei der Division ist auf die divided by zero Exception zu achten
                     {                   
                         ergebnis = input1 / input2;
                     }
@@ -58,7 +58,7 @@ namespace Mini_Taschenrechner
                     {
                         Console.Clear();
                         Console.WriteLine("Division durch 0!");
-                        goto Start;
+                        goto Start;         //Im Fall einer Division durch 0 springt das Program zur start Sprungmarke 
                     }                        
                     
                     break;
@@ -68,12 +68,12 @@ namespace Mini_Taschenrechner
                 case "-":
                     ergebnis = input1 - input2;
                     break;
-                default:
+                default:        //Wurde eien ungülltige Eingabe getätigt springt das Program zur EingabeOperator Sprungmarke und ermöglicht dem user daher eine neue Operatoreingabe
                     Console.Clear();
                     Console.WriteLine("Ungültige Eingabe! Zulässig sind * / + und -");
                     goto EingabeOperator;                   
             }
-            Console.WriteLine($"{input1} {inputOperator} {input2} = {ergebnis}");
+            Console.WriteLine($"{input1} {inputOperator} {input2} = {ergebnis}");       //Ausgabe des Ergebnises
             Console.ReadLine();
         }
     }
--- U7 Stefan/U7/U7/Program.cs
+++ U7 Stefan/U7/U7/Program.cs
@@ -10,34 +10,35 @@ namespace U7
     {
         static void Main(string[] args)
         {
-            int input;
-        Start:
+            int input;      //Programmwahl des Users
+        Start:              //Sprungmarke Start für goto Statements
             Console.Clear();
-            int i = 0;
-            Console.WriteLine(@"Wählen Sie ein Program:
+            int i = 0;      //int i als Laufvariable für 1x1 lernen
+            //Ausgabe des Menüs:
+            Console.WriteLine(@"Wählen Sie ein Program:     
 1x1 lernen              [1]
 Zufallszahl ausgeben    [2]
 Program beenden         [3]");
-            try
+            try         //Try catch zum Abfangen falscher Eingaben (non numeric)
             {
                 input = int.Parse(Console.ReadLine());
             }
-            catch (FormatException)
+            catch (FormatException) //FormatException, falls der input nicht in einen int geparst werden kann
             {
                 Console.Clear();
                 Console.WriteLine("Ungültige Eingabe, Press any key to return to start!");
-                Console.ReadLine();
-                goto Start;
+                Console.ReadLine(); 
+                goto Start;     //Program springt zur Start Sprungmarke
             }
-            switch(input)
+            switch(input)           //Switch um die Programwahl weiter zu verarbeiten
             {
-                case 1:
+                case 1:     //7er Reihe wird abgefragt
                     while (i <= 10)
                     {
                        
-                    FalscheAntwort:
+                    FalscheAntwort:     //Sprungmare FalscheAntwort
                         int antwort;
-                        try
+                        try         //Try catch um non numeric inputs abzufangen
                         {
                             Console.WriteLine($"{i} * 7 = ?");
                             antwort = int.Parse(Console.ReadLine());                           
@@ -47,7 +48,7 @@ Program beenden         [3]");
                             Console.WriteLine("Ungültige Eingabe");
                             goto FalscheAntwort;
                         }
-                        if (antwort == i * 7)
+                        if (antwort == i * 7)       //if um die Eingabe auf Richtigkeit zu prüfen!
                         {
                             Console.WriteLine("Bravo!");
                             i++;
@@ -55,24 +56,24 @@ Program beenden         [3]");
                         else
                         {
                             Console.WriteLine("Falsche Antwort... Versuch's nochmal");
-                            goto FalscheAntwort;
+                            goto FalscheAntwort;        //Falsche Antwort --> Neuer Versuch durch goto FalscheAntwort
                         }                    
                     }
                     Console.Clear();
                     Console.WriteLine("Geschafft! Du Mathekönig!!\nPress any key to continue");
-                    Console.ReadLine();
+                    Console.ReadLine();         //Hat man die 7er Reihe geschafft springt man durch eine willkürliche Eingabe zurück ins Startmenü
                     goto Start;
                     
-                case 2:
-                    Random rnd = new Random();
-                    int randomNumber = rnd.Next(1, 10);
+                case 2:     //Case 2 Ausgabe einer Zufallszahl
+                    Random rnd = new Random(); //Erstellen eines neuen Objekts der Random Klasse
+                    int randomNumber = rnd.Next(1, 10);         //Aufrufen der next Methode des Random Objekts um eine Zufallszahl zwischen 1 und 10 zu erstellen
                     Console.WriteLine($@"Zufallszahl: {randomNumber}
 Press any key to continue!");
                     Console.ReadLine();
                     goto Start;
                 case 3:
-                    return;
-                default:
+                    return;         //return beendet die Main Methode
+                default:        //Default falls zwar eine Zahl als PRogrammauswahl eingegeben wurde diese aber nicht zwischen 1 und 3 liegt
                     Console.Clear();
                     Console.WriteLine($"Ungültige Eingabe: {input}! nur Zahlen zwischen 1 und 3 zulässig!");
                     goto Start;                  
